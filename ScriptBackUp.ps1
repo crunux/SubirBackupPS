3094b623636e176ac0bfbece3fbb112bd4927c47
Cross <86730271+cr0s502@users.noreply.github.com>
2022-04-11 15:07:48 -0400

    Update ScriptBackUp.ps1

--- ScriptBackUp.ps1
+++ ScriptBackUp.ps1
@@ -14,13 +14,13 @@ Write-Host "Archivo Copiado!!";
 #Solo acepta archivos .zip
 #Expand-Archive -LiteralPath C:\Users\$env:UserName\Desktop\MyBackup.rar  -DestinationPath C:\Users\$env:UserName\Desktop\
 Write-Host "Archivo Descomprimido.!";
-#Subimos el Backup.
-&cmd /c "mysql -u root -pmysql0102 cencarci1 < C:\Users\$env:UserName\Desktop\MyBackup.sql"
+#Subimos el Backup. Ejecutamos es comando de cmd con &cmd /c 
+&cmd /c "mysql -u User -pPassword databases < C:\Users\$env:UserName\Desktop\MyBackup.sql"
 Write-Host "Backup Realizado.!";
 #Eliminamos el MyBackup.rar y Mybackup.sql
 Remove-Item C:\Users\$env:UserName\Desktop\MyBackup.rar, C:\Users\$env:UserName\Desktop\MyBackup.sql
-#Guardamos en una variable el query de mysql, el cual se convierte en arreglo. 
-$sql = mysql -uroot -pmysql0102 -e "SELECT max(fecha_pedido) FROM cencarci1.pedidos_de_internamientos;"
+#Guardamos en una variable el query de mysql, el cual se convierte en arreglo. mysql se agrega con la ruta de Patch del sistema.
+$sql = mysql -uUser -pPassword -e "SELECT max(fecolumna) database.table;"
 #Guardamos en una variable la fecha actual.
 $fecha = Get-Date -Format "yyyy-MM-dd";
 $msg = "La Fecha del ultimo Backup es: "
@@ -39,13 +39,13 @@ else{
 
 #Creamos el cuerpo del mensaje que vamos a enviar.
 $message = new-object Net.Mail.MailMessage;
-$message.From = "notifiacionesdelsistema@cencarci.com";
-$message.To.Add("soporte@cencarci.com");
+$message.From = "notifiacionesdelsistema@mail.com";
+$message.To.Add("soporte@mail.com");
 $message.Subject = $subject;
 $message.Body = $msg + $sql[1] ;
-$smtp = new-object Net.Mail.SmtpClient("mail.cencarci.com", "587");
+$smtp = new-object Net.Mail.SmtpClient("mail.mailserv.com", "587");
 $smtp.EnableSSL = $true;
-$smtp.Credentials = New-Object System.Net.NetworkCredential("notifiacionesdelsistema@cencarci.com", "s@dBread53");
+$smtp.Credentials = New-Object System.Net.NetworkCredential("notifiacionesdelsistema@mailserv.com", "Password");
 $smtp.send($message);
 write-host "Mail Enviado..!!";
 
